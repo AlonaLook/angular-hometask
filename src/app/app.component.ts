d8c93bc96381accb9ba694cf1b10cf25a25d3dc1
Alona <alona.lykavska@gmail.com>
2020-07-15 23:24:18 +0300

    Add homework for lesson #11(form validation).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+export interface IUserData {
+  name: string;
+  email: string;
+  password: string;
+  survey: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,4 +14,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('form') form: NgForm;
+
+  defaultTitle = 'student';
+  isSubmitted = false;
+  userData: IUserData;
+
+  onSubmit() {
+    this.isSubmitted = true;
+
+    this.userData = {
+      name: this.form.value.name,
+      email: this.form.value.email,
+      password: this.form.value.password,
+      survey: this.form.value.survey
+    };
+    this.form.reset();
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { HeaderComponent } from './home-page/header/header.component';
-import { LogoComponent } from './home-page/header/logo/logo.component';
-import { MenuComponent } from './home-page/header/menu/menu.component';
-import { ContentComponent } from './home-page/content/content.component';
-import { TodosComponent } from './home-page/content/todos/todos.component';
-import { TodoListComponent } from './home-page/content/todos/todo-list/todo-list.component';
-import { TodoInputComponent } from './home-page/content/todos/todo-input/todo-input.component';
-import { TodoItemComponent } from './home-page/content/todos/todo-list/todo-item/todo-item.component';
-import { FooterComponent } from './home-page/footer/footer.component';
+
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
-    HeaderComponent,
-    LogoComponent,
-    MenuComponent,
-    ContentComponent,
-    TodosComponent,
-    TodoListComponent,
-    TodoInputComponent,
-    TodoItemComponent,
-    FooterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
