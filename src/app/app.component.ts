64508f4a6c2826f7f9864db7343027f1c691af68
Alona <alona.lykavska@gmail.com>
2020-07-20 13:50:07 +0300

    Add homework for lesson #12 (reactive form).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import {Component, ViewChild} from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {CustomValidator} from '../validators/custom.validator';
 
 export interface IUserData {
   name: string;
   email: string;
   password: string;
   survey: string;
+  skills: string[];
 }
 
 @Component({
@@ -13,25 +15,72 @@ export interface IUserData {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  @ViewChild('form') form: NgForm;
+export class AppComponent implements OnInit {
+  form: FormGroup;
 
-  defaultTitle = 'student';
+  defaultTitle = 'middle';
   isSubmitted = false;
   userData: IUserData;
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        CustomValidator.prohibitedValue.bind(this)
+      ],
+        CustomValidator.anotherProhibitedValue.bind(this)
+      ),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.email
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      survey: new FormControl(this.defaultTitle, Validators.required),
+      skills: new FormArray([])
+    });
+  }
+
+  get controls() {
+    return (this.form.get('skills') as FormArray).controls;
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+
+  get isEmptyName() {
+    return this.name.errors && this.name.errors.required && this.name.touched;
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  get isPasswordShort() {
+    return this.password.errors.minlength && this.password.errors.minlength.actualLength < this.password.errors.minlength.requiredLength;
+  }
+  get survey() {
+    return this.form.get('survey');
+  }
+
   onSubmit() {
     this.isSubmitted = true;
-    const {name, email, password, survey } = {...this.form.value};
-
-    this.userData = {
-      name,
-      email,
-      password,
-      survey
-    };
+    this.userData = {...this.form.value};
+    console.log('data', this.userData);
     this.form.reset();
   }
 
 
+  addSkill() {
+    const control = new FormControl('', Validators.required);
+    (this.form.get('skills') as FormArray).push(control);
+  }
 }
